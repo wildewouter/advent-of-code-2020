55a7f770e5964437dc4eef5c90e6558639ab81fd
Wouter de Wild <wouter@codewild.nl>
2020-12-04 20:52:15 +0100

    Day 4 complete!

--- src/day4.ts
+++ src/day4.ts
@@ -1,13 +1,13 @@
 import fs from "fs";
 
 type Document = {
-    byr?: string,
-    iyr?: string,
-    eyr?: string,
-    hgt?: string,
-    hcl?: string,
-    ecl?: string,
-    pid?: string,
+    byr: string,
+    iyr: string,
+    eyr: string,
+    hgt: string,
+    hcl: string,
+    ecl: string,
+    pid: string,
     cid?: string,
 }
 
@@ -36,3 +36,47 @@ const onValidDocument = (document: Document): boolean =>
 
 
 console.log('Part one: ', documents.filter(onValidDocument).length);
+
+interface ValidateDocumentPropertyFunc {
+    (value: string): boolean;
+}
+
+const daRules: {
+    [k in keyof Document]: ValidateDocumentPropertyFunc
+} = {
+    byr: (value: string) => 1920 <= +value && +value <= 2002,
+    iyr: (value: string) => 2010 <= +value && +value <= 2020,
+    eyr: (value: string) => 2020 <= +value && +value <= 2030,
+    hgt: (value: string) => {
+        const [, number, identifier] = value.match(/^([0-9]+)([a-z]*)/) as [string, string, string];
+        if (number === undefined || identifier === undefined) {
+            return false;
+        }
+
+        const parsedNumber = +number;
+
+        return identifier === 'cm' ? 150 <= parsedNumber && parsedNumber <= 193
+            : identifier === 'in' ? 59 <= parsedNumber && parsedNumber <= 76
+                : false;
+    },
+    hcl: (value: string) => Boolean(value.match(/^#[0-9a-f]{6}$/)),
+    ecl: (value: string) => Boolean(value.match(/^(amb|blu|brn|gry|grn|hzl|oth)$/)),
+    pid: (value: string) => Boolean(value.match(/^[0-9]{9}$/)),
+    cid: () => true,
+}
+
+const onDaRules = (document: Document) =>
+    !Object.keys(document)
+        .filter((key): key is keyof Document => true)
+        .map(key => ({func: daRules[key], key}))
+        .map(
+            ({func, key}) => {
+                const value = document[key];
+
+                return (func !== undefined && value !== undefined)
+                    ? func(value)
+                    : false
+            }
+        ).includes(false);
+
+console.log('Part two: ', documents.filter(onValidDocument).filter(onDaRules).length);
