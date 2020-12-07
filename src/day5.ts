7a967b80c2e6028f15f06878e9d69f6a865b574e
Wouter de Wild <wouter@codewild.nl>
2020-12-08 00:13:19 +0100

    Part two! Answer for all the rows with missing seats

--- src/day5.ts
+++ src/day5.ts
@@ -23,8 +23,19 @@ const seats = fs.readFileSync(__dirname + '/day5-input', 'utf8')
                             ? [result[0], result[1], result[2] + (8 / Math.pow(2, index - 6)), result[3]]
                             : result,
             [0, 127, 0, 7])
-    )
-    .map(ranges => ranges[0] * 8 + ranges[2])
-    .sort((a, b) => b - a);
+    );
 
-console.log('Part one: ', seats[0]);
+console.log('Part one: ', seats
+    .map(ranges => ranges[0] * 8 + ranges[2])
+    .sort((a, b) => b - a)[0]
+);
+
+console.log('Part two: ', Object.entries(seats
+    .sort((a, b) => a[0] - b[0])
+    .reduce((result, next) => ({
+        ...result,
+        [next[0]]: [...(result[next[0]] ?? []), next[2]]
+            .sort((a, b) => a - b),
+    }), {} as { [key: number]: number[] }))
+    .filter(([, seats]) => seats.length < 8)
+);
