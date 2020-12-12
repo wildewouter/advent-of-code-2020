5a27b51a65b0be190ec4ee32c07f16ffac90d8d1
Wouter de Wild <wouter@codewild.nl>
2020-12-12 16:33:36 +0100

    Dayumn that was part two

--- src/day6.ts
+++ src/day6.ts
@@ -2,6 +2,8 @@ import fs from "fs";
 
 const intoGroups = '\n\n';
 
+const answers = fs.readFileSync(__dirname + '/day6-input', 'utf8');
+
 const toNumberOfAnswers = (characters: string) =>
     Object.keys(
         characters
@@ -9,10 +11,31 @@ const toNumberOfAnswers = (characters: string) =>
             .reduce((result, yes: string) => ({...result, [yes]: true}), {})
     ).length;
 
-const answerPartOne = fs.readFileSync(__dirname + '/day6-input', 'utf8')
+const toSummed = (result: number = 0, next: number) => result + next;
+
+const answerPartOne = answers
     .split(intoGroups)
     .map(groups => groups.split('\n').join(''))
     .map(toNumberOfAnswers)
-    .reduce((result, next) => result + next, 0);
+    .reduce(toSummed);
 
 console.log(`Part one: ${answerPartOne}`)
+
+const answerGroupsPerRespondentToBinaryInt = (value: string) =>
+    value
+        .split('')
+        .map(character => parseInt(character, 36) - 10)
+        .reduce((result, next) => result | (1 << next), 0);
+
+const answerPartTwo = answers
+    .split(intoGroups)
+    .map(groups => groups
+        .split('\n')
+        .filter(char => char != '')
+        .map(answerGroupsPerRespondentToBinaryInt)
+    )
+    .map(group => group.reduce((previous, next) => previous & next, parseInt('11111111111111111111111111', 2)))
+    .map(lettersBinary => (lettersBinary.toString(2).match(/1/g) || []).length)
+    .reduce(toSummed)
+
+console.log(`Part two: ${answerPartTwo}`)
